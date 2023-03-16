37919bb346681e88212ca32287167253b1a45af7
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-03-16 10:41:53 +0200

    Create operate function

--- js/app.js
+++ js/app.js
@@ -14,3 +14,24 @@ function divide(a, b) {
   if (b == 0) return 'ERROR';
   return a / b;
 }
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+':
+      return sum(a, b);
+    // break;
+    case '-':
+      return subtract(a, b);
+    // break;
+    case '*':
+      return multiply(a, b);
+    // break;
+    case '/':
+      return divide(a, b);
+    // break;
+    default:
+      return 'NOT AN OPERATION';
+  }
+}
+
+console.log(operate('*', 5, 10));
