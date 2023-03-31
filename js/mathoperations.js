055e3223631551826db1b0a79b370475216084bf
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-03-31 10:48:36 +0300

    Remove commented code from the operate function

--- js/mathoperations.js
+++ js/mathoperations.js
@@ -19,16 +19,12 @@ function operate(operator, a, b) {
   switch (operator) {
     case 'add':
       return add(a, b);
-    // break;
     case 'subtract':
       return subtract(a, b);
-    // break;
     case 'multiply':
       return multiply(a, b);
-    // break;
     case 'divide':
       return divide(a, b);
-    // break;
     default:
       return a;
   }
